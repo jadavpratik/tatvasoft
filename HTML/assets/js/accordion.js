c44118660291dead1bdb1af8e9cf0e3d63d17d2b
Zrough <54739706+jadavpratik@users.noreply.github.com>
2022-01-06 15:46:59 +0530

    Change image_folder_path

--- HTML/assets/js/accordion.js
+++ HTML/assets/js/accordion.js
@@ -1,5 +1,5 @@
-const right_arrow = './assets/img/general/static/faq/right_arrow.png';
-const down_arrow = './assets/img/general/static/faq/down_arrow.png';
+const angle_right_circle = './assets/img/buttons/angle/angle_right_circle.png';
+const angle_down_circle = './assets/img/buttons/angle/angle_down_circle.png';
 
 // ACCORDION IMPLEMENTATION...
 var acc_btn = document.getElementsByClassName("accordion_btn");
@@ -11,12 +11,12 @@ for (let i = 0; i < acc_btn.length; i++) {
 		var acc_content = this.nextElementSibling;
 		if(acc_content.classList.contains('d_none')){
 			// CHANGE RIGHT_ARROW -> DOWN_ARROW
-			acc_btn[i].children[0].src = down_arrow;
+			acc_btn[i].children[0].src = angle_down_circle;
 			acc_content.classList.remove('d_none');
 		}
 		else{
 			// CHANGE DOWN_ARROW -> RIGHT_ARROW
-			acc_btn[i].children[0].src = right_arrow;
+			acc_btn[i].children[0].src = angle_right_circle;
 			acc_content.classList.add('d_none');
 		}
 	});
