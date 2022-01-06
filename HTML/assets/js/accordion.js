ccbd5a1c55b7659b82b6df2e0daa8b90cbab26b9
Zrough <54739706+jadavpratik@users.noreply.github.com>
2022-01-06 18:04:24 +0530

    Admin_Portal_Changes

--- HTML/assets/js/accordion.js
+++ HTML/assets/js/accordion.js
@@ -1,5 +1,7 @@
 const angle_right_circle = './assets/img/buttons/angle/angle_right_circle.png';
 const angle_down_circle = './assets/img/buttons/angle/angle_down_circle.png';
+const angle_right_icon = `<i class="fas fa-angle-right"></i>`;
+const angle_down_icon = `<i class="fas fa-angle-down"></i>`;
 
 // ACCORDION IMPLEMENTATION...
 var acc_btn = document.getElementsByClassName("accordion_btn");
@@ -11,11 +13,13 @@ for (let i = 0; i < acc_btn.length; i++) {
 		var acc_content = this.nextElementSibling;
 		if(acc_content.classList.contains('d_none')){
 			// CHANGE RIGHT_ARROW -> DOWN_ARROW
+			acc_btn[i].children[0].outerHTML = angle_down_icon;
 			acc_btn[i].children[0].src = angle_down_circle;
 			acc_content.classList.remove('d_none');
 		}
 		else{
 			// CHANGE DOWN_ARROW -> RIGHT_ARROW
+			acc_btn[i].children[0].outerHTML = angle_right_icon;
 			acc_btn[i].children[0].src = angle_right_circle;
 			acc_content.classList.add('d_none');
 		}
