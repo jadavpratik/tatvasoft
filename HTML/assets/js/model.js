9100b0c12c7c4840149bcbbe1d59f15c7874ee34
Zrough <54739706+jadavpratik@users.noreply.github.com>
2022-01-07 18:59:32 +0530

    Forgot Password

--- HTML/assets/js/model.js
+++ HTML/assets/js/model.js
@@ -1,8 +1,21 @@
 // --------------OPEN_MODEL------------------
-$('#open_login_popup').click(()=>{
+$('.open_login_popup').click(()=>{
     $('.model').fadeIn(100, ()=>{
         $('.model').removeClass('d_none');
+        $('.sidenav').animate({'right':'-250px'}, 500);
+        $('.forgot_password_popup').addClass('d_none');
         $('.login_popup').removeClass('d_none');
+        $('.backlight_container').addClass('backlight');
+        $('body').css({'overflow-y':'hidden'});
+    });
+});
+
+$('.open_forgot_password_popup').click(()=>{
+    $('.model').fadeIn(100, ()=>{
+        $('.model').removeClass('d_none');
+        $('.sidenav').animate({'right':'-250px'}, 500);
+        $('.forgot_password_popup').removeClass('d_none');
+        $('.login_popup').addClass('d_none');
         $('.backlight_container').addClass('backlight');
         $('body').css({'overflow-y':'hidden'});
     });
