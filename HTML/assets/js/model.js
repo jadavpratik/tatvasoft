440a0c6bf602c6d4a683d07b35ded1c4b3cc91b8
Zrough <54739706+jadavpratik@users.noreply.github.com>
2022-01-07 17:48:20 +0530

    Login Popup

--- HTML/assets/js/model.js
+++ HTML/assets/js/model.js
@@ -1,3 +1,14 @@
+// --------------OPEN_MODEL------------------
+$('#open_login_popup').click(()=>{
+    $('.model').fadeIn(100, ()=>{
+        $('.model').removeClass('d_none');
+        $('.login_popup').removeClass('d_none');
+        $('.backlight_container').addClass('backlight');
+        $('body').css({'overflow-y':'hidden'});
+    });
+});
+
+// ---------------CLOSE_MODEL------------------
 $('.model_close_btn').click(()=>{
     $('.backlight_container').removeClass('backlight');
     $('.model').addClass('d_none');
