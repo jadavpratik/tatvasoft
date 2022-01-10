1bb4021ffb81b76814446eb68ddbe9fba9a4624a
Zrough <54739706+jadavpratik@users.noreply.github.com>
2022-01-10 18:55:26 +0530

    Accordion Animation

--- HTML/assets/js/accordion.js
+++ HTML/assets/js/accordion.js
@@ -4,15 +4,20 @@ const acc_content = document.getElementsByClassName("accordion_content");
 
 for (let i = 0; i < acc_btn.length; i++) {
 
-	acc_btn[i].addEventListener("click", function() {
-		
+	acc_btn[i].addEventListener("click", function() {		
 		const panel = acc_content[i];
+		var btn_icon;// RIGHT OR DOWN ANGLE BUTTON
+		if(acc_btn[i].getElementsByTagName('img')[0]!==undefined)
+			btn_icon = acc_btn[i].getElementsByTagName('img')[0];
+		else
+			btn_icon = acc_btn[i].getElementsByTagName('i')[0];
+	
 		if(panel.style.maxHeight){//close
-			acc_btn[i].children[0].style = 'transform:rotate(0deg)';
+			btn_icon.style = `transform:rotate(0deg)`
 			panel.style.maxHeight = null;
 		}
 		else{//open
-			acc_btn[i].children[0].style = 'transform:rotate(90deg)';
+			btn_icon.style = `transform:rotate(90deg)`;
 			panel.style.maxHeight = panel.scrollHeight + 'px';
 		}
 	});
