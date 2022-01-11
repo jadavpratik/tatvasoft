98be04a25e24daa2240c8a26bf1e02c7ed4ae84c
Zrough <54739706+jadavpratik@users.noreply.github.com>
2022-01-11 11:58:41 +0530

    Dropdown Issue Solve

--- HTML/assets/js/dropdown.js
+++ HTML/assets/js/dropdown.js
@@ -1,9 +1,9 @@
-const dropdown_btn = document.getElementsByClassName('dropdown_btn');
-const dropdown_menu = document.getElementsByClassName('dropdown_menu');
+// const dropdown_btn = document.getElementsByClassName('dropdown_btn');
+// const dropdown_menu = document.getElementsByClassName('dropdown_menu');
 
-for(let i=0; i<dropdown_btn.length; i++){
-    dropdown_btn[i].addEventListener('click', ()=>{
-        // $('.dropdown_menu').addClass('d_none');
-        dropdown_menu[i].classList.toggle('d_none');
-    });
-}
+// for(let i=0; i<dropdown_btn.length; i++){
+//     dropdown_btn[i].addEventListener('click', ()=>{
+//         // $('.dropdown_menu').addClass('d_none');
+//         dropdown_menu[i].classList.toggle('d_none');
+//     });
+// }
