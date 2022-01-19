d85f5b1b2d682671aed4701c115e6e0134ef6d9f
Zrough <54739706+jadavpratik@users.noreply.github.com>
2022-01-19 12:07:53 +0530

    Set Customer & Service Provider Tabs

--- HTML/assets/js/tabletab.js
+++ HTML/assets/js/tabletab.js
@@ -5,7 +5,9 @@ for(let i=0; i<table_tab_btn.length; i++){
     table_tab_btn[i].addEventListener('click', ()=>{
         $('.table_tab_btn').removeClass('active_table_tab');
         $('.table_tab_content').addClass('d_none');
-        table_tab_btn[i].classList.add('active_table_tab');
+        if(i!==0){
+            table_tab_btn[i].classList.add('active_table_tab');
+        }
         table_tab_content[i].classList.remove('d_none');
     });
 }
